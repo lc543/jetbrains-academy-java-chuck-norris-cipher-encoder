78b200321502267b5d0b1d01efd59d440be1e0bc
lc543 <41060382+lc543@users.noreply.github.com>
2023-02-28 18:40:19 +0700

    complete stage 1

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,7 +1,29 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string:");
+        String userInput = scanner.nextLine();
+        String result = separateCharactersBySpace(userInput);
+        System.out.println(result);
+    }
+
+    private static String separateCharactersBySpace(String userInput) {
+        if (userInput.length() <= 1) {
+            return userInput;
+        }
+        char[] stringChars = userInput.toCharArray();
+        char[] temp = new char[stringChars.length*2-1];
+        for (int i = 0; i < stringChars.length; i++){
+            temp[2*i] = stringChars[i];
+        }
+        for (int i = 0; i < stringChars.length-1; i++){
+            temp[2*i+1] = ' ';
+        }
+        return new String(temp);
     }
 }
