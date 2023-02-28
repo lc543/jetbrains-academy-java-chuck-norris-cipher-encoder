b37321b0ee71e9fcdb78956c7397b5a1aca53e34
lc543 <41060382+lc543@users.noreply.github.com>
2023-02-28 19:49:30 +0700

    complete stage 3

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,5 +1,7 @@
 package chucknorris;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,23 +10,48 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input string:");
         String userInput = scanner.nextLine();
-        String[] binaryFormStrings = convertToBinaryFormString(userInput);
-        System.out.println("The result:");
-        for (int i = 0; i < userInput.length();i++){
-            System.out.printf(
-                    "%c = %s%n",userInput.charAt(i),binaryFormStrings[i]
-            );
-        }
+        String encodedText = encryptChuckNorris(userInput);
+        System.out.println("The result:\n" + encodedText);
     }
 
-    private static String[] convertToBinaryFormString(String userInput) {
-        String[] result = new String[userInput.length()];
-        for (int i = 0;i< userInput.length(); i++){
-            char chr = userInput.charAt(i);
-            String binaryFormat = String.format("%7s",Integer.toBinaryString(chr)).replace(' ','0');
-            result[i]=binaryFormat;
+    private static String encryptChuckNorris(String text) {
+        String binaryString = String.join("", convertToBinaryString(text));
+        List<String> encodedBitStreaks = encodeChuckNorris(binaryString);
+        return String.join(" ",encodedBitStreaks);
+    }
+
+    private static List<String> encodeChuckNorris(String binaryString){
+        List<String> encodedBitStreaks = new ArrayList<>();
+        char previousBit = binaryString.charAt(0);
+        int streakSize = 1;
+        for (int i = 1; i < binaryString.length(); i++) {
+            char currentBit = binaryString.charAt(i);
+            if (currentBit == previousBit) {
+                streakSize++;
+            } else {
+                encodedBitStreaks.add(encodeBitStreak(previousBit, streakSize));
+                previousBit = currentBit;
+                streakSize = 1;
+            }
+        }
+        encodedBitStreaks.add(encodeBitStreak(previousBit, streakSize)); // process the last character
+        return encodedBitStreaks;
+    }
+
+    private static String encodeBitStreak(char bit, int streakSize) {
+        String firstBlock = bit=='0'?"00":"0";
+        String secondBlock = "0".repeat(streakSize);
+        return  firstBlock + " " + secondBlock;
+    }
+
+
+    private static String[] convertToBinaryString(String text) {
+        String[] result = new String[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            char chr = text.charAt(i);
+            String binaryFormat = String.format("%7s", Integer.toBinaryString(chr)).replace(' ', '0');
+            result[i] = binaryFormat;
         }
         return result;
     }
-
 }
